c931d1ff5aa0b586cc7cf667dd3233060152eb53
andrelmonteiro <andrelmonteirose@gmail.com>
2020-12-27 23:31:00 -0300

    Projeto criado  adicionado a classe categoria

---
+++ src/test/java/com/andre/cursomc/Cursomc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andre.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cursomc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
